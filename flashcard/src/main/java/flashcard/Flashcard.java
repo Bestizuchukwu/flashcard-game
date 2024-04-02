79e54475cf082972b0a7adf655270d1f57322ebd
Nwakaeze Izuchukwu Best <besti2019@163.com>
2024-04-02 03:14:32 +0100

    checking if the argument given is empty or help

--- flashcard/src/main/java/flashcard/Flashcard.java
+++ flashcard/src/main/java/flashcard/Flashcard.java
@@ -3,14 +3,22 @@ import org.apache.commons.cli.*;
 import java.io.File;
 
 
-/**
- * Hello world!
- *
- */
+
 public class Flashcard 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Options options = createOptions();
+        CommandLIneParser parser = new DefaultParser();
+        try {
+
+            CommandLine cmd = parser.parse(options, args);
+            if (cmd.hasOption("help") || args.length == 0){
+                HelpFormatter formatter = new HelpFormatter();
+                formatter.printHelp("flashcard <card-file> [options]", options);
+                system.exit(0);
+            }
+            
+        }
     }
 }
